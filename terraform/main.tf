f670173ae54cd1d273045ab2a0b1dd3d4334c43b
Terrence <tpryan@google.com>
2023-05-12 13:29:33 -0700

    fix: getting lbv to work with new api.

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,41 +1,70 @@
-
-data "google_project" "project" {
-  project_id = var.project_id
-}
-
-
+/**
+ * Copyright 2021 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 locals {
   exemplar_machine_type = "e2-medium"
   node_machine_type     = "e2-micro"
+  subnet_name           = "${var.deployment_name}-subnet-01"
+  custom_network        = var.network_id != ""
+  network_id            = local.custom_network ? var.network_id : module.vpc[0].network_id
+  subnet_self_link      = var.subnet_self_link != "" ? var.subnet_self_link : module.vpc[0].subnets["${var.region}/${local.subnet_name}"].self_link
+  network_project_id    = var.network_project_id != "" ? var.network_project_id : var.project_id
+  lb_endpoint           = "http://${module.gce-lb-http.external_ip}/"
 }
 
 # Enabling services in your GCP project
-variable "gcp_service_list" {
-  description = "The list of apis necessary for the project"
-  type        = list(string)
-  default = [
-    "compute.googleapis.com",
+module "project-services" {
+  source                      = "terraform-google-modules/project-factory/google//modules/project_services"
+  version                     = "~> 13.0"
+  disable_services_on_destroy = false
+
+  project_id  = var.project_id
+  enable_apis = var.enable_apis
+
+  activate_apis = [
+    "compute.googleapis.com"
   ]
 }
 
-resource "google_project_service" "all" {
-  for_each                   = toset(var.gcp_service_list)
-  project                    = data.google_project.project.number
-  service                    = each.key
-  disable_dependent_services = false
-  disable_on_destroy         = false
-}
+module "vpc" {
+  count   = local.custom_network ? 0 : 1
+  source  = "terraform-google-modules/network/google"
+  version = "~> 4.0"
 
-resource "google_compute_network" "main" {
-  project = var.project_id
-  name = "${var.basename}-network"
-}
+  project_id   = var.project_id
+  network_name = "${var.deployment_name}-network"
+  routing_mode = "GLOBAL"
 
+  subnets = [
+    {
+      subnet_name   = local.subnet_name
+      subnet_ip     = "10.10.10.0/24"
+      subnet_region = var.region
+    }
+  ]
+
+  depends_on = [
+    module.project-services
+  ]
+
+}
 
 resource "google_compute_firewall" "private-allow-ssh" {
-  name    = "${var.basename}-allow-ssh"
-  project = var.project_id
-  network = google_compute_network.main.id
+  name    = "${var.deployment_name}-allow-ssh"
+  project = local.network_project_id
+  network = local.network_id
 
   allow {
     protocol = "tcp"
@@ -45,43 +74,54 @@ resource "google_compute_firewall" "private-allow-ssh" {
   source_ranges = ["0.0.0.0/0"]
 
   target_tags = ["private-ssh"]
-}  
+}
 
+resource "google_compute_firewall" "allow_healthchecks" {
+  name    = "${var.deployment_name}-allow-healthchecks"
+  project = local.network_project_id
+  network = local.network_id
+  direction     = "INGRESS"
+  source_ranges = ["35.191.0.0/16", "209.85.152.0/22", "209.85.204.0/22"]
+  allow {
+    protocol = "tcp"
+  }
+# TODO change this before pushing through
+  target_tags = ["private-ssh"]
+}
 
+data "local_file" "index" {
+  filename = "${path.module}/../files/index.html"
 
-
+}
 
 # Create Instance Exemplar on which to base Managed VMs
 resource "google_compute_instance" "exemplar" {
-  name         = "${var.basename}-exemplar"
+  name         = "${var.deployment_name}-exemplar"
   machine_type = local.exemplar_machine_type
   zone         = var.zone
   project      = var.project_id
+  labels       = var.labels
 
   tags                    = ["http-server", "private-ssh"]
   metadata_startup_script = "apt-get update -y \n apt-get install nginx -y \n  printf '${data.local_file.index.content}'  | tee /var/www/html/index.html \n chgrp root /var/www/html/index.html \n chown root /var/www/html/index.html \n chmod +r /var/www/html/index.html"
+
   boot_disk {
     auto_delete = true
-    device_name = "${var.basename}-exemplar"
+    device_name = "${var.deployment_name}-exemplar"
     initialize_params {
-      image = "family/ubuntu-1804-lts"
+      image = "family/debian-10"
       size  = 200
       type  = "pd-standard"
     }
   }
 
   network_interface {
-    network = google_compute_network.main.id
+    subnetwork         = local.subnet_self_link
+    subnetwork_project = local.network_project_id
     access_config {
       // Ephemeral public IP
     }
   }
-
-  depends_on = [google_project_service.all]
-}
-
-data "local_file" "index" {
-  filename = "${path.module}/../code/index.html"
 }
 
 resource "time_sleep" "startup_completion" {
@@ -89,34 +129,37 @@ resource "time_sleep" "startup_completion" {
   depends_on      = [google_compute_instance.exemplar]
 }
 
-resource "google_compute_snapshot" "snapshot" {
+resource "google_compute_snapshot" "main" {
   project           = var.project_id
-  name              = "${var.basename}-snapshot"
-  source_disk       = google_compute_instance.exemplar.boot_disk[0].source
+  name              = "${var.deployment_name}-snapshot"
+  source_disk       = google_compute_instance.exemplar.boot_disk[0].device_name
   zone              = var.zone
-  storage_locations = ["${var.region}"]
+  storage_locations = [var.region]
   depends_on        = [time_sleep.startup_completion]
+  labels            = var.labels
 }
 
 # Create Disk Image for Instance Template
 resource "google_compute_image" "exemplar" {
   project         = var.project_id
-  name            = "${var.basename}-latest"
-  family          = var.basename
-  source_snapshot = google_compute_snapshot.snapshot.self_link
-  depends_on      = [google_compute_snapshot.snapshot]
+  name            = "${var.deployment_name}-latest"
+  family          = var.deployment_name
+  source_snapshot = google_compute_snapshot.main.self_link
+  depends_on      = [google_compute_snapshot.main]
+  labels          = var.labels
 }
 
 # Create Instance Template
-resource "google_compute_instance_template" "default" {
+resource "google_compute_instance_template" "main" {
   project     = var.project_id
-  name        = "${var.basename}-template"
+  name        = "${var.deployment_name}-template"
   description = "This template is used to create app server instances."
-  tags        = ["httpserver"]
+  tags        = ["http-server", "private-ssh"]
+  labels      = var.labels
 
   metadata_startup_script = "sed -i.bak \"s/{{NODENAME}}/$HOSTNAME/\" /var/www/html/index.html"
 
-  instance_description = "BasicLB node"
+  instance_description = "${var.deployment_name} node"
   machine_type         = local.node_machine_type
   can_ip_forward       = false
 
@@ -128,15 +171,21 @@ resource "google_compute_instance_template" "default" {
   }
 
   network_interface {
-    network = google_compute_network.main.id
+    subnetwork         = local.subnet_self_link
+    subnetwork_project = local.network_project_id
   }
 
-  depends_on = [google_compute_image.exemplar]
+}
+
+resource "google_compute_target_pool" "main" {
+  project = var.project_id
+  name    = "${var.deployment_name}-target-pool"
+  region  = var.region
 }
 
 resource "google_compute_health_check" "autohealing" {
   project             = var.project_id
-  name                = "${var.basename}-autohealing-health-check"
+  name                = "${var.deployment_name}-autohealing-health-check"
   check_interval_sec  = 5
   timeout_sec         = 5
   healthy_threshold   = 2
@@ -149,16 +198,21 @@ resource "google_compute_health_check" "autohealing" {
 }
 
 # Create Managed Instance Group
-resource "google_compute_instance_group_manager" "default" {
+resource "google_compute_instance_group_manager" "main" {
   project            = var.project_id
-  name               = "${var.basename}-mig"
+  provider           = google-beta
+  name               = "${var.deployment_name}-mig"
   zone               = var.zone
   target_size        = var.nodes
-  base_instance_name = "${var.basename}-mig"
-
+  base_instance_name = "${var.deployment_name}-mig"
+  target_pools       = [google_compute_target_pool.main.id]
 
   version {
-    instance_template = google_compute_instance_template.default.id
+    instance_template = google_compute_instance_template.main.id
+  }
+
+  all_instances_config {
+    labels = var.labels
   }
 
   named_port {
@@ -166,31 +220,18 @@ resource "google_compute_instance_group_manager" "default" {
     port = "80"
   }
 
-  depends_on = [google_compute_instance_template.default]
-}
-
-# Creating External IP
-resource "google_compute_global_address" "default" {
-  project    = var.project_id
-  name       = "${var.basename}-ip"
-  ip_version = "IPV4"
-}
-
-# Standing up Load Balancer
-resource "google_compute_health_check" "http" {
-  project = var.project_id
-  name    = "${var.basename}-health-chk"
-
-  tcp_health_check {
-    port = "80"
+  auto_healing_policies {
+    health_check      = google_compute_health_check.autohealing.id
+    initial_delay_sec = 300
   }
+
 }
 
 resource "google_compute_autoscaler" "main" {
   project = var.project_id
-  name    = "${var.basename}-autoscaler"
+  name    = "${var.deployment_name}-autoscaler"
   zone    = var.zone
-  target  = google_compute_instance_group_manager.default.id
+  target  = google_compute_instance_group_manager.main.id
 
   autoscaling_policy {
     max_replicas    = var.nodes * 3
@@ -203,51 +244,81 @@ resource "google_compute_autoscaler" "main" {
   }
 }
 
-resource "google_compute_firewall" "allow-health-check" {
-  project       = var.project_id
-  name          = "allow-health-check"
-  network       = google_compute_network.main.id
-  source_ranges = ["130.211.0.0/22", "35.191.0.0/16"]
+module "gce-lb-http" {
+  source  = "GoogleCloudPlatform/lb-http/google"
+  version = "~> 6.3"
 
-  allow {
-    protocol = "tcp"
-    ports    = ["80"]
-  }
-}
-
-resource "google_compute_backend_service" "default" {
-  project               = var.project_id
-  name                  = "${var.basename}-service"
-  load_balancing_scheme = "EXTERNAL"
-  protocol              = "HTTP"
-  port_name             = "http"
-  backend {
-    group = google_compute_instance_group_manager.default.instance_group
-  }
-
-  health_checks = [google_compute_health_check.http.id]
-}
-
-resource "google_compute_url_map" "lb" {
-  project         = var.project_id
-  name            = "${var.basename}-lb"
-  default_service = google_compute_backend_service.default.id
-}
-
-# Enabling HTTP
-resource "google_compute_target_http_proxy" "default" {
   project = var.project_id
-  name    = "${var.basename}-lb-proxy"
-  url_map = google_compute_url_map.lb.id
+  name    = "${var.deployment_name}-lb"
+
+  firewall_networks = [local.network_id]
+
+  backends = {
+    default = {
+      description                     = null
+      protocol                        = "HTTP"
+      port                            = "80"
+      port_name                       = "http"
+      timeout_sec                     = 10
+      enable_cdn                      = false
+      custom_request_headers          = null
+      custom_response_headers         = null
+      security_policy                 = null
+      connection_draining_timeout_sec = null
+      session_affinity                = null
+      affinity_cookie_ttl_sec         = null
+
+
+
+      health_check = {
+        check_interval_sec  = null
+        timeout_sec         = null
+        healthy_threshold   = null
+        unhealthy_threshold = null
+        request_path        = "/"
+        port                = "80"
+        name                = "${var.deployment_name}-health-chk"
+        host                = null
+        logging             = null
+      }
+
+      log_config = {
+        enable      = true
+        sample_rate = 1.0
+      }
+
+      groups = [
+        {
+          group                        = google_compute_instance_group_manager.main.instance_group
+          balancing_mode               = null
+          capacity_scaler              = null
+          description                  = null
+          max_connections              = null
+          max_connections_per_instance = null
+          max_connections_per_endpoint = null
+          max_rate                     = null
+          max_rate_per_instance        = null
+          max_rate_per_endpoint        = null
+          max_utilization              = null
+        },
+      ]
+
+      iap_config = {
+        enable               = false
+        oauth2_client_id     = null
+        oauth2_client_secret = null
+      }
+    }
+  }
 }
 
-resource "google_compute_forwarding_rule" "google_compute_forwarding_rule" {
-  project               = var.project_id
-  name                  = "${var.basename}-http-lb-forwarding-rule"
-  provider              = google-beta
-  region                = var.region
-  load_balancing_scheme = "EXTERNAL"
-  port_range            = "80"
-  target                = google_compute_target_http_proxy.default.id
-  ip_address            = google_compute_global_address.default.id
+data "http" "check" {
+  url = local.lb_endpoint
+  retry {
+    attempts     = 50
+    max_delay_ms = 10 * 1000
+    min_delay_ms = 5 * 1000
+  }
+
+  depends_on = [module.gce-lb-http]
 }
--- terraform/output.tf
+++ terraform/output.tf
@@ -1,6 +1,39 @@
+/**
+ * Copyright 2021 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 # Waiting for website to be serving http
-output "endpoint" {
-  value       = google_compute_global_address.default.address
+output "load_balancer_endpoint" {
+  value       = local.lb_endpoint
   description = "The url of the front end which we want to surface to the user"
+
+  precondition {
+    condition     = data.http.check.status_code != "200"
+    error_message = "LB is not serving yet"
+  }
+}
+
+
+output "endpoint" {
+  value       = module.gce-lb-http.external_ip
+  description = "The IP of the load balancer"
+
+}
+
+# Output loadbalancer details
+output "console_page_for_load_balancer" {
+  value       = "https://console.cloud.google.com/net-services/loadbalancing/details/http/${var.deployment_name}-lb-url-map?project=${var.project_id}"
+  description = "The url of the load balancer page in console"
 }
